473a582faa7f05f5125cebca956a102e07dd4504
shubhsbankar <60430028+shubhsbankar@users.noreply.github.com>
2024-02-22 18:50:56 +0000

    Learned conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,46 @@
+let score = null;
+
+let convertedScore = Number(score)
+//console.log(typeof convertedScore);
+//console.log(convertedScore);
+
+// null => 0
+// true => 1 , false => 0
+// "33abc" => NaN (Not a Number)
+// "33" => 33
+// "33.86" => 33.86
+// undefined => NaN
+// "Shubham" => NaN
+
+let isLoggedIn =87.5
+
+let isLogedInBoolean = Boolean(isLoggedIn)
+// console.log(typeof isLogedInBoolean);
+// console.log(isLogedInBoolean);
+
+// -123 => true
+// 123 => true
+// 0 => false
+// null => false
+// undefined => false
+// NaN => false
+// "" => false
+// "Shubham" => true
+// 87.5 => true
+
+let data = false
+
+let dataString = String(data)
+
+console.log(typeof dataString);
+console.log(dataString);
+
+// NaN => NaN
+// undefined => undefined
+// null => null
+// 765 => 765
+// 876.9876 => 876.9876
+// true => true
+// false => false
+
+
