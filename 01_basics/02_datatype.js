ed544de6f321adba08f54bc3b62a27d71d6f31be
shubhsbankar <60430028+shubhsbankar@users.noreply.github.com>
2024-02-22 18:12:37 +0000

    Learned datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as per newer version
+
+//alert(3+3); // we are using nodejs not browser
+
+
+let name = "Shubham"
+let age = 18
+let isLoggedIn = true
+
+
+// number => range is 2 to the power of 53
+// bigint => beyond the number range
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null); // => object
+console.log(typeof undefined); // => undefined
